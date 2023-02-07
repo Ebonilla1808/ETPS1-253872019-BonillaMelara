003d8c0cead1f4f29209f4a62129b3e19215a810
Ebonilla1808 <67129217+Ebonilla1808@users.noreply.github.com>
2023-02-06 20:05:58 -0600

    Creando tablas de multiplicar

--- Controles/app/src/main/java/sv/edu/utec/controles/MainActivity.java
+++ Controles/app/src/main/java/sv/edu/utec/controles/MainActivity.java
@@ -3,12 +3,28 @@ package sv.edu.utec.controles;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edValor1, edtResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edValor1 = findViewById(R.id.edtTabla);
+        edtResultado = findViewById(R.id.edtResultado);
+    }
+
+    public void Tabla(View v){
+        int valTabla = Integer.parseInt(edValor1.getText().toString());
+        String resultado="";
+        edtResultado.setText(resultado);
+        for (int i = 1; i<=10; i++){
+            resultado = String.valueOf(valTabla * i);
+            edtResultado.append(valTabla +" * "+i + " = " + resultado+"\n");
+        }
     }
 }
