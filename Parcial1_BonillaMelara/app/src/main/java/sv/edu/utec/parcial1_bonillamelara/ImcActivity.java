338d6428d223cf6e944d15f66e814292e0c44459
Ebonilla1808 <67129217+Ebonilla1808@users.noreply.github.com>
2023-02-22 20:03:37 -0600

    creando segunda pantalla

---
+++ Parcial1_BonillaMelara/app/src/main/java/sv/edu/utec/parcial1_bonillamelara/ImcActivity.java
@@ -0,0 +1,13 @@
+package sv.edu.utec.parcial1_bonillamelara;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ImcActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_imc);
+    }
+}
