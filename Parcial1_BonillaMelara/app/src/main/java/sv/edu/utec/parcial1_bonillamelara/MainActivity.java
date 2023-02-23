b5fe28a99c399b1b8af101d3c41ae534b1ed0f12
Ebonilla1808 <67129217+Ebonilla1808@users.noreply.github.com>
2023-02-22 19:31:04 -0600

    creando primer layout

---
+++ Parcial1_BonillaMelara/app/src/main/java/sv/edu/utec/parcial1_bonillamelara/MainActivity.java
@@ -0,0 +1,16 @@
+package sv.edu.utec.parcial1_bonillamelara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    String usuario= "parcialETps1", pass="p4rC14l#tp$";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
