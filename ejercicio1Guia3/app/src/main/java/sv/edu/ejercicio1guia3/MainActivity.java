5976eb321b696d955c3521e4ecae92e7993e7b5f
Ramirez20 <70790682+Ramirez20@users.noreply.github.com>
2023-03-19 20:48:25 -0600

    Completando ejercicio 1 Guia 3

--- ejercicio1Guia3/app/src/main/java/sv/edu/ejercicio1guia3/MainActivity.java
+++ ejercicio1Guia3/app/src/main/java/sv/edu/ejercicio1guia3/MainActivity.java
@@ -3,12 +3,42 @@ package sv.edu.ejercicio1guia3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Declaracion de variables
+
+    EditText edtValor;
+    TextView txvrResultado;
+    Button btnCalculo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtValor=findViewById(R.id.edValor);
+        txvrResultado=findViewById(R.id.txvResultado);
+        btnCalculo=findViewById(R.id.btnCalcular);
+    }
+    public void calculo (View v) {
+        try {
+            int numero = Integer.parseInt(edtValor.getText().toString());
+
+            if(numero > 0) {
+                txvrResultado.setText("El número es positivo");
+            } else if(numero == 0) {
+                txvrResultado.setText("El número es nulo");
+            } else {
+                txvrResultado.setText("El número es negativo");
+            }
+
+        }
+        catch (Exception e){
+            txvrResultado.setText("El valor ingresado no es valido. Porfavor, ingrese un numero entero");
+        }
     }
 }
