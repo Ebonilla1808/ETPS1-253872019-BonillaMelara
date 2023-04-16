04be3ee98f260edac345199ede8cdbbd1c119f55
Ebonilla1808 <67129217+Ebonilla1808@users.noreply.github.com>
2023-04-16 14:29:29 -0600

    actualizando guia

---
+++ ListPersonas_GUIA3/app/src/main/java/sv/edu/utec/listpersonas_guia3/AdaptadorPersonas.java
@@ -0,0 +1,34 @@
+package sv.edu.utec.listpersonas_guia3;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class AdaptadorPersonas extends ArrayAdapter<Personas> {
+    AppCompatActivity appCompatActivity;
+    ArrayList<Personas> listapersonas;
+    Integer [] imgPersonas;
+    AdaptadorPersonas(AppCompatActivity context, ArrayList<Personas> listapersonas, Integer [] imgPersonas) {
+        super(context, R.layout.persona, listapersonas);
+        appCompatActivity = context;
+        this.listapersonas = listapersonas;
+        this.imgPersonas = imgPersonas;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = appCompatActivity.getLayoutInflater();
+        View item = inflater.inflate(R.layout.persona, null);
+        TextView textView1 = item.findViewById(R.id.tvNombre);
+        textView1.setText(listapersonas.get(position).getNombre());
+        ImageView imageView1 = item.findViewById(R.id.imvFoto);
+        imageView1.setImageResource(imgPersonas[position]);
+        return (item);
+    }
+}
---
+++ ListPersonas_GUIA3/app/src/main/java/sv/edu/utec/listpersonas_guia3/Personas.java
@@ -0,0 +1,20 @@
+package sv.edu.utec.listpersonas_guia3;
+
+
+public class Personas {
+    private String nombre;
+    private char genero;
+
+    public Personas(String nombre, char genero) {
+        this.nombre = nombre;
+        this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+}
