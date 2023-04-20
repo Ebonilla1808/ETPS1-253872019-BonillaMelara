d274787951fc61c57ee9199459799c8a527558be
Ebonilla1808 <67129217+Ebonilla1808@users.noreply.github.com>
2023-04-19 21:18:06 -0600

    Agregando funcionaldiad de insertar

--- Parcial3_BonillaMelara/app/src/main/java/sv/edu/utec/parcial3_bonillamelara/MainActivity.java
+++ Parcial3_BonillaMelara/app/src/main/java/sv/edu/utec/parcial3_bonillamelara/MainActivity.java
@@ -6,18 +6,23 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import sv.edu.utec.parcial3_bonillamelara.Helper.AdminSQLiteOpenHelper;
 import sv.edu.utec.parcial3_bonillamelara.clases.AddFragment;
 import sv.edu.utec.parcial3_bonillamelara.clases.ContactFragment;
 import sv.edu.utec.parcial3_bonillamelara.clases.HomeFragment;
@@ -26,15 +31,17 @@ import sv.edu.utec.parcial3_bonillamelara.clases.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView btnNav;
+    Button btnInsert;
+    EditText edtNombre, edtApellido, edtCorreo, edtTelefono, edtCodigo;
     private FrameLayout fragmentCont;
     private ImageView btnAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); btnNav=findViewById(R.id.btnNav);
+        setContentView(R.layout.activity_main);
+        btnNav=findViewById(R.id.btnNav);
         fragmentCont = findViewById(R.id.fragmentCont);
         btnAdd = findViewById(R.id.btnAdd);
-
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,11 +51,14 @@ public class MainActivity extends AppCompatActivity {
                 AddFragment fragment = new AddFragment(); // Instancia del fragmento que deseas cargar
                 fragmentTransaction.replace(R.id.fragmentCont, fragment);
                 fragmentTransaction.commit();
+                btnInsert = findViewById(R.id.btnInsert);
             }
         });
 
+
         btnNav.setOnNavigationItemSelectedListener((BottomNavigationView.OnNavigationItemSelectedListener) navaListener);
     }
+
     private BottomNavigationView.OnNavigationItemSelectedListener navaListener= new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
--- Parcial3_BonillaMelara/app/src/main/java/sv/edu/utec/parcial3_bonillamelara/clases/AddFragment.java
+++ Parcial3_BonillaMelara/app/src/main/java/sv/edu/utec/parcial3_bonillamelara/clases/AddFragment.java
@@ -1,20 +1,71 @@
 package sv.edu.utec.parcial3_bonillamelara.clases;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import sv.edu.utec.parcial3_bonillamelara.Helper.AdminSQLiteOpenHelper;
 import sv.edu.utec.parcial3_bonillamelara.R;
 
 public class AddFragment extends Fragment {
+
+    private Button btnInsert;
+
+    EditText edtNombre, edtApellido, edtCorreo, edtTelefono, edtCodigo;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add,container,false);
+        View view = inflater.inflate(R.layout.fragment_add, container, false);
+
+        btnInsert = view.findViewById(R.id.btnInsert);
+        edtNombre= view.findViewById(R.id.edtNombre);
+        edtApellido= view.findViewById(R.id.edtApellido);
+        edtCodigo= view.findViewById(R.id.edtCod);
+        edtCorreo= view.findViewById(R.id.edtCorreo);
+        edtTelefono= view.findViewById(R.id.edtTelefono);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(getActivity(),"parcial",null,1);
+                SQLiteDatabase bd= admin.getWritableDatabase();
+                String cod = edtCodigo.getText().toString();
+                String nombre = edtNombre.getText().toString();
+                String apellido = edtApellido.getText().toString();
+                String correo = edtCorreo.getText().toString();
+                String telefono = edtTelefono.getText().toString();
+                ContentValues informacion =new ContentValues();
+                informacion.put("idContacto", cod);
+                informacion.put("Nombre", nombre);
+                informacion.put("Apellidos", apellido);
+                informacion.put("Teléfono", telefono);
+                informacion.put("Correo", correo);
+                try {
+                    bd.insert("Contactos", null, informacion);
+
+                    Toast.makeText(getActivity(), "Se inserto el contacto", Toast.LENGTH_LONG).show();
+                    bd.close();
+
+                } catch (Exception e){
+                    //pendiente imprimir error
+                    Toast.makeText(getActivity(), ""+e.getCause(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+        return view;
     }
+
 }
